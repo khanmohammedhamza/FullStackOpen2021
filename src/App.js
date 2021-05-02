91bec947a3cc358031fec69b860e93f7439aac24
khanmohammedhamza <17.10.2000hamza@gmail.com>
2021-05-03 03:44:45 +0530

    please work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
 import React from 'react'
+
+const Header= (props) => {
+  return(
+<h1> 
+        {props.course}
+      </h1>
+  )
+}
+
+const Content= (props) => {
+  return(
+    <>
+  <p>{props.part1} {props.exercise1}</p>
+  <p>{props.part2} {props.exercise2}</p>
+  <p>{props.part3} {props.exercise3}</p>
+  </>
+  )
+}
+
+const Total= (props) => {
+  return(
+    <p>
+   number of exercises= {props.exercise1} 
+   </p>
+  )
+}
 const App = () => {
   const course  = 'Half Stack appliation development'
   const part1 = 'fundamentals of React'
@@ -7,24 +33,18 @@ const App = () => {
   const exercise2 = 7
   const part3 = 'State of a component'
   const exercise3= 14
+  
   return(
     <div>
-      <h1>got 
-        {course}
-      </h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>
-        Number of exercises {exercise1 + exercise2 + exercise3}
-      </p>
+      <Header course={course} />
+      <Content part1={part1} exercise1={exercise1} part2={part2} exercise2={exercise2} part3={part3} exercise3={exercise3}/>
+    
+      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+     <Total exercise1={exercise1 + exercise2 + exercise3} />
+      
+      
     </div>
   )
 }
 export default App
+
