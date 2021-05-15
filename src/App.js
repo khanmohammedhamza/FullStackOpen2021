23bad98ae443a3aecf209f75bd16658f968101e1
khanmohammedhamza <17.10.2000hamza@gmail.com>
2021-05-15 21:14:36 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import React from 'react'
 const App = () => {
   const course = 'Half Stack application development'
   console.log(course)
-  const part1 = {
+  const parts= [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
 
   return (
     <div>
- <p>{course} </p> 
- <p>{part1.name} {part1.exercises}</p>
- <p>{part2.name} {part2.exercises}</p>
- <p>{part3.name} {part3.exercises}</p>
-
+<p>{course}</p>
+<p>
+parts.forEach((name,exercises)=> {
+  
+});
+</p>
 
     
     </div>
