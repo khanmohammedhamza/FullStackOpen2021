c36dac2a3d9902d6b2a65e52f6bfe91e86d441b1
khanmohammedhamza <17.10.2000hamza@gmail.com>
2021-05-20 00:01:10 +0530

    the basic man basic thats where we stuck

--- src/App.js
+++ src/App.js
@@ -6,33 +6,38 @@ const Header= (props) => {
       </h1>
   )
 }
-const Parts= (props) =>
-{
+const Parts= (props) =>{
+  console.log(props.exercises)
   return(
     <div> 
     {props.name} {props.exercises}
     </div>
   )
 } 
-const Total= (props) =>
-{
-  return(
-    <div>
-      {props.parts.map(x =><Parts exercises={x.exercises}/>  )}
-    </div>
-  )
-}
-const Content= (props) =>
-{
-  console.log( props.parts)
- return(
+ const Content = (props) =>
+  {
 
-   <div>
-  {props.parts.map(value => <Parts name ={ value.name}  exercises ={value.exercises}key = {value.name} /> )}
-   </div>
- )
+   return (
+     <div>
+      {props.parts.map(value => <Parts name = {value.name} exercises = {value.exercises} key = {value.name}/> )}
+     </div> 
+   )
  }
- 
+  const Total= (props) => {
+   console.log(Sum(props.parts))
+     return ( 
+<div>
+    Total = {Sum(props.parts)}
+</div>
+     )
+   
+ } 
+ const Sum = (parts) => {
+   return(parts[0].exercises + parts[1].exercises + parts[2].exercises)} 
+ const x = Sum
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   
@@ -50,16 +55,17 @@ const App = () => {
       exercises: 14
     }
   ]
-
-
+  
+  const [Just,To,Understand] = parts
+   console.log(Just) 
+   console.log(parts)
 return (
   <div>
     <Header course={course} />
     <Content parts ={parts}/>
-    <Total parts ={parts}/>
-    
-   
+    <Total parts={parts} />
   </div>
+  
 )
 } 
 export default App
